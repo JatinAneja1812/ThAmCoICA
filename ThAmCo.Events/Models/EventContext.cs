1a9cd9f2cffd0a4fe1fda6bbb44efd253f183655
W9359412 <70910485+JatinAneja2000@users.noreply.github.com>
2021-11-17 21:34:38 +0000

    Added Staff Controller and data Model

--- ThAmCo.Events/Models/EventContext.cs
+++ ThAmCo.Events/Models/EventContext.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ThAmCo.Events.Models;
 
 namespace ThAmCo.Events.Models
 {
     public class EventContext : DbContext
     {
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Staff> Staff { get; set; }
 
         public EventContext(DbContextOptions<EventContext> options)
         : base(options)
@@ -29,6 +31,25 @@ namespace ThAmCo.Events.Models
                 new Customer { CustomerId = 5, FirstName = "Jennie", LastName = "White", TelePhoneNumber = "0766839432", EmailId = "Jennie.non.White200@gmail.com" }
                 );
 
+            builder.Entity<Staff>()
+                .HasData(
+                new Staff {  Staffid = 1, FirstName ="Paulo" , LastName = "Marks", StaffType = "Waiter"},
+                new Staff { Staffid = 2, FirstName = "Mary", LastName = "Gibbs", StaffType = "Manager" },
+                new Staff { Staffid = 3, FirstName = "Kacy", LastName = "Holland", StaffType = "Wedding Planner" },
+                new Staff { Staffid = 4, FirstName = "Arvind", LastName = "Sharma", StaffType = "Bartender" },
+                new Staff { Staffid = 5, FirstName = "Raghav", LastName = "Kuma", StaffType = "Event Organiser" },
+                new Staff { Staffid = 6, FirstName = "Kyle", LastName = "Butler", StaffType = "Photographer" },
+                new Staff { Staffid = 7, FirstName = "Andy", LastName = "Angels", StaffType = "Caterer" },
+                new Staff { Staffid = 8, FirstName = "Mandy", LastName = "Green", StaffType = "DJ Music Mixer" },
+                new Staff { Staffid = 9, FirstName = "Sandy", LastName = "Geller", StaffType = "Waiter" },
+                new Staff { Staffid = 10, FirstName = "Barry", LastName = "Tribbiani", StaffType = "Waiter" },
+                new Staff { Staffid = 11, FirstName = "Penny", LastName = "Parks", StaffType = "Event Organiser" },
+                new Staff { Staffid = 12, FirstName = "Larc", LastName = "Meads", StaffType = "Caterer" },
+                new Staff { Staffid = 13, FirstName = "Garry", LastName = "James", StaffType = "Waiter" },
+                new Staff { Staffid = 14, FirstName = "Kirti", LastName = "Sanon", StaffType = "Photographer" }
+                );
+
         }
+        
     }
 }
